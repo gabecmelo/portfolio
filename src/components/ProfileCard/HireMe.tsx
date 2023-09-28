b839cfb926af2c8f4975d1507128e5e5ed369f19
Gabriel Cabral Melo <contatogabemelo@gmail.com>
2023-09-28 16:02:16 -0300

    add hire-me option

--- src/components/ProfileCard/HireMe.tsx
+++ src/components/ProfileCard/HireMe.tsx
@@ -1,6 +1,10 @@
+import { AiOutlineMail } from 'react-icons/ai'
+
 function HireMe() {
   return (
-    <div>Contate-me!</div>
+    <div className="flex items-center justify-center gap-3 text-l bg-green-primary text-black text-center rounded-full h-14 hover-hire">
+          <AiOutlineMail className="text-xl" /> CONTATE-ME!
+    </div>
   )
 }
 
